a7edd8880072750350ad708b3561193fcc356e36
Dhairya2505 <dhairyasingla250504@gmail.com>
2024-05-04 18:31:19 +0530

    minor fixes

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -52,12 +52,6 @@ const ChatAppPage = () => {
         };
         
     },[]);
-    
-    const sendMessage = () => {
-        socket.emit("send-message",{ Message : message, usernamefrom:FromUsername, usernameto:ToUsername });
-        setChats(prevChats => [...prevChats, { usernamefrom:FromUsername, usernameto:ToUsername, Message:message }]);
-        setMessage('');
-    }
 
     return (
         <div className="h-svh">
@@ -66,32 +60,37 @@ const ChatAppPage = () => {
             </div>
 
             <div className="fixed pt-20 pb-24 flex flex-col p-5 bg-black/95 w-full h-full overflow-y-auto border border-white">
-                
                 {
+                    
                     chats.map((e,i) => {
                         if(e.usernamefrom == FromUsername){
-                            return <div className="m-1 self-end bg-gray-400 p-2 rounded-md max-w-sm md:max-w-md lg:max-w-lg" key={i}>
+                             return (<div className="m-1 self-end bg-gray-400 p-2 rounded-md max-w-sm md:max-w-md lg:max-w-lg" key={i}>
                                 {`${e.Message}`}
-                            </div>
+                            </div>)
                         }
                         else{
-                            return <div className="m-1 self-start bg-gray-300 p-2 rounded-md max-w-sm md:max-w-md lg:max-w-lg" key={i}>
+                            return (<div className="m-1 self-start bg-gray-300 p-2 rounded-md max-w-sm md:max-w-md lg:max-w-lg" key={i}>
                                 <div className="text- font-bold font-serif">
                                     {e.usernamefrom}
                                 </div>
                                 <div className="font-serif pl-1">
                                     {e.Message}
                                 </div>
-                            </div>
+                            </div>)
                         }
                     })
                 }
 
             </div>
-            <div className="fixed bottom-0 w-full flex h-20 items-center justify-center bg-black border border-white">
+            <form onSubmit={ (e) => {
+                e.preventDefault();
+                socket.emit("send-message",{ Message : message, usernamefrom:FromUsername, usernameto:ToUsername });
+                setChats(prevChats => [...prevChats, { usernamefrom:FromUsername, usernameto:ToUsername, Message:message }]);
+                setMessage('');
+            } } className="fixed bottom-0 w-full flex h-20 items-center justify-center bg-black border border-white">
                 <input type="text" value={message} onChange={ (e) => { setMessage(e.target.value) } } className="shadow-sm shadow-white outline-none text-gray-200 w-full p-2 h-12 rounded-lg my-2 ml-2 mr-1 border border-white bg-black" />
-                <button className=" h-12 w-20 my-2 mr-2 ml-1 active:shadow-inner active:shadow-white shadow-sm shadow-white rounded-md border border-white text-white select-none cursor-pointer" onClick={ sendMessage }>Send</button>
-            </div>
+                <button type="submit" className=" h-12 w-20 my-2 mr-2 ml-1 active:shadow-inner active:shadow-white shadow-sm shadow-white rounded-md border border-white text-white select-none cursor-pointer">Send</button>
+            </form>
         </div>
     )
 }
