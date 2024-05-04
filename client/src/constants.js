005d34096d0df806ba4bc687cee9a335f6a56418
Dhairya2505 <dhairyasingla250504@gmail.com>
2024-05-04 20:06:55 +0530

    URL updated

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_CHAT_SERVER = 'http://localhost:7001';
-export const BACKEND_URI = 'http://localhost:8001';
+export const BACKEND_CHAT_SERVER = 'https://baat-cheet-chat-backend.onrender.com';
+export const BACKEND_URI = 'https://baat-cheet-backend-29l7.onrender.com';
